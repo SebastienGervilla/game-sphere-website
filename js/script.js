87f62c8f849dfe077bbe53ae4a409061099dd8b4
Skaaz <92156546+SebastienGervilla@users.noreply.github.com>
2021-11-15 08:49:20 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,26 @@
+window.onscroll = function(){updateScrollBar()};
+
+function updateScrollBar() {
+    docYPos = scrollY;
+    scrollBar = document.getElementById('scroll-bar');
+    scrollbarYPos = scrollBar.getBoundingClientRect().y,
+    scrollBarHeight = scrollBar.offsetHeight;
+    progressBarHeight = -scrollbarYPos + window.innerHeight / 2;
+    if (progressBarHeight < 0) {
+        progressBarHeight = 0;
+    } else if (progressBarHeight > scrollBarHeight) {
+        progressBarHeight = scrollBarHeight;
+    } else {
+        progressBarHeight = Math.round(progressBarHeight);
+    }
+
+    document.getElementById('progress-bar').style.height = progressBarHeight.toString() + 'px';
+
+
+    landingAreaYPos = document.getElementById('landing-area').style.height;
+    if (docYPos > landingAreaYPos + 100) {
+        document.getElementById('side-bar').classList.add('active');
+    } else {
+        document.getElementById('side-bar').classList.remove('active');
+    }
+}
