6a8369d7c03354750c8f784cd5f73f9577b9dc81
Skaaz <92156546+SebastienGervilla@users.noreply.github.com>
2021-11-21 18:22:28 +0100

    Add files via upload

--- Game Sphere/js/script.js
+++ Game Sphere/js/script.js
@@ -1,24 +1,39 @@
 window.onscroll = function(){updateScrollBar()};
 
+isFull = false
 function updateScrollBar() {
     docYPos = scrollY;
     scrollBar = document.getElementById('scroll-bar');
     scrollbarYPos = scrollBar.getBoundingClientRect().y,
     scrollBarHeight = scrollBar.offsetHeight;
     progressBarHeight = -scrollbarYPos + window.innerHeight / 2;
-    if (progressBarHeight < 0) {
-        progressBarHeight = 0;
-    } else if (progressBarHeight > scrollBarHeight) {
+    // if (!(isFull)) {
+    //     if (progressBarHeight < 0) {
+    //         progressBarHeight = 0;
+    //     } else if (progressBarHeight >= scrollBarHeight) {
+    //         progressBarHeight = scrollBarHeight;
+    //         isFull = true;
+    //     } else {
+    //         progressBarHeight = Math.round(progressBarHeight);
+    //     }
+    //     document.getElementById('progress-bar').style.height = progressBarHeight.toString() + 'px';
+    // }
+
+    if (isFull) {
         progressBarHeight = scrollBarHeight;
+    }
+    else if (progressBarHeight < 0) {
+        progressBarHeight = 0;
+    } else if (progressBarHeight >= scrollBarHeight) {
+        progressBarHeight = scrollBarHeight;
+        isFull = true;
     } else {
         progressBarHeight = Math.round(progressBarHeight);
     }
-
     document.getElementById('progress-bar').style.height = progressBarHeight.toString() + 'px';
 
-
     landingAreaYPos = document.getElementById('landing-area').style.height;
-    if (docYPos > landingAreaYPos + 100) {
+    if (docYPos > landingAreaYPos + window.innerHeight) {
         document.getElementById('side-bar').classList.add('active');
     } else {
         document.getElementById('side-bar').classList.remove('active');
